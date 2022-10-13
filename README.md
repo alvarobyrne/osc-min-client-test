46610030fd863af003dfeabaaed827b76d58545f
Alvaro Obyrne <alvarobyrne@gmail.com>
2022-10-13 17:43:55 -0500

    envelope sent from SC and received in js
    envelope sent from supercollider and received in javascript
    
    received in node-js

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # osc-min-client-test
 testing osc-min for dosisWeb
+
+## send envelope via osc from supercollider
+
+- sender: `envelope-send.scd`
+- receiver: `supercolliderReceiver.js`
+
+based on/ learnt from @elifieldsteel 's youtube channel video [Week 10: Audio Coding with SuperCollider - Fall 2017 MUS 499C](https://www.youtube.com/watch?t=2740&v=dbmtrijhosk) at which the [`SendReply` UGen](http://doc.sccode.org/Classes/SendReply.html) is used.
--- supercolliderReceiver.js
+++ supercolliderReceiver.js
@@ -1,12 +1,31 @@
-const udp = require('dgram');
+/**
+ * see envelope-send.scd
+ */
+const udp = require("dgram");
 // const udp = dgram.createSocket("udp4");
-const osc = require('osc-min');
+const osc = require("osc-min");
 const inport = "localhost";
-const sock = udp.createSocket("udp4", function(msg, rinfo) {
-    var error, error1;
-    let rawMessage = osc.fromBuffer(msg);
-    console.log('rawMessage: ', rawMessage);
-    return;
-  });
-   
-  sock.bind(3333,inport);
+const sock = udp.createSocket("udp4", function (msg, receivingInfo) {
+  // console.log("receivingInfo: ", receivingInfo);
+  let rawMessage = osc.fromBuffer(msg);
+  // console.log("msg: ", msg);
+  /**
+   * If the messages are sent as in `envelope-send.sc` file, hte following is the shape of the object received
+   * In supercollider ~testNetAddr.sendMsg("envelope", msg[3])
+   * shape of rawMeassage = {
+   *  address:'envelope',
+   *  args:[
+   *   {
+   *    type:'float'
+   *    value: <Buffer ... >
+   *   }
+   *  ],
+   *  oscType:'message'
+   * }
+   */
+  console.log("rawMessage: ", rawMessage.args[0].value);
+  // console.log("rawMessage: ", rawMessage);
+  value = rawMessage.args[0].value;
+});
+
+sock.bind(3333, inport);
